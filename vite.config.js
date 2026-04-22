cc5c8a6c500fcef8eed5da4486fe42b77d015c95
Muthukumar Sakthivel <smuthukumar1014@gmail.com>
2026-04-22 11:47:24 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'client/dist',
+    outDir: 'dist',
     emptyOutDir: true,
   },
   server: {
